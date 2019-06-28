ed5f70d5b01cbb5100c2df3ea6af403a71b0516a
xiaobobo <994301552@qq.com>
2019-06-28 16:22:35 +0800

    ???321

--- git-test-2/src/com/xiaobobo/test/GitTest2.java
+++ git-test-2/src/com/xiaobobo/test/GitTest2.java
@@ -4,9 +4,7 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("gittest2");
         System.out.println("gittest2");
-        System.out.println("gittest2");
-        System.out.println("gittest2");
-        System.out.println("gittest2");
+        System.out.println("hahahahahah");
         System.out.println("gittest2");
         System.out.println("gittest2");
     }

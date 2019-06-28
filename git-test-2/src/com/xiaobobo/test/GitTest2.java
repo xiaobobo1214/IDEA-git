1b4dbdd7b6a0c469a6ea000801ed046e13fab7a6
xiaobobo <994301552@qq.com>
2019-06-28 16:14:22 +0800

    本地提交测试

--- git-test-2/src/com/xiaobobo/test/GitTest2.java
+++ git-test-2/src/com/xiaobobo/test/GitTest2.java
@@ -3,5 +3,11 @@ package com.xiaobobo.test;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("gittest2");
+        System.out.println("gittest2");
+        System.out.println("gittest2");
+        System.out.println("gittest2");
+        System.out.println("gittest2");
+        System.out.println("gittest2");
+        System.out.println("gittest2");
     }
 }
